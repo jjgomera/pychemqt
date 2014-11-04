d626d5fc8a66e0ba611090dcc4e540a6d2586e62
jjgomera <jjgomera@gmail.com>
2014-11-04 22:51:33 +0100

    improve saturation convergence in meos, rejected akasaka method

--- lib/meos.py
+++ lib/meos.py
@@ -842,59 +842,49 @@ class MEoS(_fase):
 
         fase.mu = self._Viscosity(fase.rho, self.T, fase)
         fase.k = self._ThCond(fase.rho, self.T, fase)
-        fase.nu = unidades.Diffusivity(fase.mu/fase.rho)
-        fase.alfa = unidades.Diffusivity(fase.k/1000/fase.rho/fase.cp)
-        fase.Prandt = unidades.Dimensionless(fase.mu*fase.cp*1000/fase.k)
+        if fase.mu:
+            fase.nu = unidades.Diffusivity(fase.mu/fase.rho)
+        else:
+            fase.nu = None
+        if fase.k:
+            fase.alfa = unidades.Diffusivity(fase.k/1000/fase.rho/fase.cp)
+        else:
+            fase.alfa = None
+        if fase.mu and fase.k:
+            fase.Prandt = unidades.Dimensionless(fase.mu*fase.cp*1000/fase.k)
+        else:
+            fase.Prandt = None
         fase.epsilon = unidades.Dimensionless(self._Dielectric(fase.rho, self.T))
 
     def _saturation(self, T=None):
-        """Akasaka (2008) "A Reliable and Useful Method to Determine the Saturation State from Helmholtz Energy Equations of State", Journal of Thermal Science and Technology, 3, 442-451
-        http://dx.doi.org/10.1299/jtst.3.442"""
+        """Saturation calculation for two phase search"""
         if not T:
             T = self.T
 
-        Ps = self._Vapor_Pressure(T)
-        rhoL = self._Liquid_Density(T)
-        rhoG = self._Vapor_Density(T, Ps)
-        g = 1000.
-        erroro = 1e6
-        rholo = rhoL
-        rhogo = rhoG
-        contador = 0
-        while True:
-            contador += 1
-            deltaL = rhoL/self.rhoc
-            deltaG = rhoG/self.rhoc
-            liquido = self._eq(rhoL, T)
-            vapor = self._eq(rhoG, T)
+        rhoLo = self._Liquid_Density(T)
+        rhoGo = self._Vapor_Density(T)
+        
+        def f(parr):
+            rhol, rhog = parr
+            deltaL = rhol/self.rhoc
+            deltaG = rhog/self.rhoc
+            liquido = self._eq(rhol, T)
+            vapor = self._eq(rhog, T)
             Jl = deltaL*(1+deltaL*liquido["fird"])
             Jv = deltaG*(1+deltaG*vapor["fird"])
             Kl = deltaL*liquido["fird"]+liquido["fir"]+log(deltaL)
             Kv = deltaG*vapor["fird"]+vapor["fir"]+log(deltaG)
-            Jdl = 1+2*deltaL*liquido["fird"]+deltaL**2*liquido["firdd"]
-            Jdv = 1+2*deltaG*vapor["fird"]+deltaG**2*vapor["firdd"]
-            Kdl = 2*liquido["fird"]+deltaL*liquido["firdd"]+1/deltaL
-            Kdv = 2*vapor["fird"]+deltaG*vapor["firdd"]+1/deltaG
-            Delta = Jdv*Kdl-Jdl*Kdv
-            error = abs(Kv-Kl)+abs(Jv-Jl)
+            return Kv-Kl, Jv-Jl
 
-            if error < 1e-12 or contador > 100:
-                break
-            elif error > erroro:
-                rhoL = rholo
-                rhoG = rhogo
-                g = g/2.
-            else:
-                erroro = error
-                rholo = rhoL
-                rhogo = rhoG
-                rhoL = rhoL+g/Delta*((Kv-Kl)*Jdv-(Jv-Jl)*Kdv)
-                rhoG = rhoG+g/Delta*((Kv-Kl)*Jdl-(Jv-Jl)*Kdl)
-        if error > 1e-3:
-            print("Iteration don´t converge, residual error %g" % error)
+        rhoL, rhoG = fsolve(f, [rhoLo, rhoGo])
         if rhoL == rhoG:
             Ps = self.Pc
         else:
+            liquido = self._eq(rhoL, T)
+            vapor = self._eq(rhoG, T)
+            deltaL = rhoL/self.rhoc
+            deltaG = rhoG/self.rhoc
+
             Ps = self.R*T*rhoL*rhoG/(rhoL-rhoG)*(liquido["fir"]-vapor["fir"]+log(deltaL/deltaG))
         return rhoL, rhoG, Ps
 
