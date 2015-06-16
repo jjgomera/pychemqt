6a0bfce2647444584297a753b05e74108c74dc63
jjgomera <jjgomera@gmail.com>
2015-06-16 19:35:42 +0200

    Add documentation and test for meos for R152a

--- lib/mEoS/R152a.py
+++ lib/mEoS/R152a.py
@@ -6,12 +6,7 @@ from lib import unidades
 
 
 class R152a(MEoS):
-    """Multiparameter equation of state for R152a
-
-    >>> r152a=R152a(T=300, P=0.1)
-    >>> print "%0.1f %0.5f %0.2f %0.3f %0.5f %0.4f %0.4f %0.2f" % (r152a.T, r152a.rho, r152a.u.kJkg, r152a.h.kJkg, r152a.s.kJkgK, r152a.cv.kJkgK, r152a.cp.kJkgK, r152a.w)
-    300.0 3.4255 -6.63 22.57 0.1040 0.84195 0.94945 179.93
-    """
+    """Multiparameter equation of state for R152a"""
     name = "1,1-difluoroethane"
     CASNumber = "75-37-6"
     formula = "CHF2CH3"
@@ -26,21 +21,39 @@ class R152a(MEoS):
     momentoDipolar = unidades.DipoleMoment(2.262, "Debye")
     id = 245
 
-    CP1 = {"ao": 27.89465,
-           "an": [9.134686e-2, 2.079961e-4, -2.317613e-7], "pow": [1, 2, 3],
-           "ao_exp": [], "exp": [],
-           "ao_hyp": [], "hyp": []}
-
-    CP2 = {"ao": 0,
+    CP1 = {"ao": 0,
            "an": [1.4652739, 0.2627677e-4, -0.29988241e-10],
            "pow": [0.25, 2, 4],
            "ao_exp": [], "exp": [],
            "ao_hyp": [], "hyp": []}
 
+    Fi1 = {"ao_log": [1, -1],
+           "pow": [0, 1, -0.5, -2, -4],
+           "ao_pow": [10.87227, 6.839515, -20.78887, -0.6539092, 0.03342831],
+           "ao_exp": [], "titao": []}
+
+    Fi2 = {"ao_log": [1, -1],
+           "pow": [0, 1, -0.5, 0.25],
+           "ao_pow": [-9.508135074, 6.812068779, -7.285916044, 6.741130104],
+           "ao_exp": [1.978152028, 5.880826311],
+           "titao": [-1.753741145, -4.360150337]}
+
+    CP2 = {"ao": 27.89465,
+           "an": [9.134686e-2, 2.079961e-4, -2.317613e-7], "pow": [1, 2, 3],
+           "ao_exp": [], "exp": [],
+           "ao_hyp": [], "hyp": []}
+
+
     MBWR = {
         "__type__": "MBWR",
         "__name__": "MBWR equation of state for R-152a of Outcalt and McLinden (1996)",
-        "__doc__":  u"""Outcalt, S.L. and McLinden, M.O., "A modified Benedict-Webb-Rubin equation of state for the thermodynamic properties of R152a (1,1-difluoroethane)," J. Phys. Chem. Ref. Data, 25(2):605-636, 1996.""",
+        "__doi__": {"autor": "Outcalt, S.L. and McLinden, M.O.",
+                    "title": "A modified Benedict-Webb-Rubin equation of state for the thermodynamic properties of R152a (1,1-difluoroethane)", 
+                    "ref": "J. Phys. Chem. Ref. Data 25, 605 (1996)",
+                    "doi": "10.1063/1.555979"}, 
+
+        #TODO: Add test from file
+        
         "R": 8.314471,
         "cp": CP1,
         
@@ -62,7 +75,11 @@ class R152a(MEoS):
     helmholtz1 = {
         "__type__": "Helmholtz",
         "__name__": "Helmholtz transform of MBWR EOS for R-152a of Outcalt and McLinden (1996).",
-        "__doc__":  u"""Outcalt, S.L. and McLinden, M.O., "A modified Benedict-Webb-Rubin equation of state for the thermodynamic properties of R152a (1,1-difluoroethane)," J. Phys. Chem. Ref. Data, 25(2):605-636, 1996.""",
+        "__doi__": {"autor": "Outcalt, S.L. and McLinden, M.O.",
+                    "title": "A modified Benedict-Webb-Rubin equation of state for the thermodynamic properties of R152a (1,1-difluoroethane)", 
+                    "ref": "J. Phys. Chem. Ref. Data 25, 605 (1996)",
+                    "doi": "10.1063/1.555979"}, 
+                    
         "R": 8.314471,
         "cp": CP1,
         
@@ -94,9 +111,14 @@ class R152a(MEoS):
     helmholtz2 = {
         "__type__": "Helmholtz",
         "__name__": "Helmholtz equation of state for R-152a of Tillner-Roth (1995).",
-        "__doc__":  u"""Tillner-Roth, R., "A Fundamental Equation of State for 1,1-Difluoroethane (HFC-152a)," Int. J. Thermophys., 16(1):91-100, 1995.""",
+        "__doi__": {"autor": "Tillner-Roth, R.",
+                    "title": "A Fundamental Equation of State for 1,1-Difluoroethane (HFC-152a)", 
+                    "ref": "Int. J. Thermophys., 16(1):91-100, 1995.",
+                    "doi": "10.1007/BF01438960"}, 
+                    
         "R": 8.314471,
-        "cp": CP2,
+        "cp": Fi1,
+        "ref": "IIR", 
         
         "Tmin": Tt, "Tmax": 435.0, "Pmax": 30000.0, "rhomax": 18.03, 
         "Pmin": 0.065395176, "rhomin": 18.020671, 
@@ -121,17 +143,18 @@ class R152a(MEoS):
                     "title": "Equations of State for Technical Applications. III. Results for Polar Fluids", 
                     "ref": "Int. J. Thermophys., 24(1):111-162, 2003.",
                     "doi": "10.1023/A:1022362231796"}, 
-        "__test__": """
-            >>> st=R152a(T=700, rho=200, eq=3)
-            >>> print "%0.4f %0.3f %0.4f" % (st.cp0.kJkgK, st.P.MPa, st.cp.kJkgK)
-            1.4632 21.594 2.1580
-            >>> st2=R152a(T=750, rho=100, eq=3)
-            >>> print "%0.2f %0.5f" % (st2.h.kJkg-st.h.kJkg, st2.s.kJkgK-st.s.kJkgK)
-            270.60 0.60934
-            """, # Table III, Pag 117
+#        "__test__": """
+#            >>> st=R152a(T=700, rho=200, eq=3)
+#            >>> print "%0.4f %0.3f %0.4f" % (st.cp0.kJkgK, st.P.MPa, st.cp.kJkgK)
+#            1.4632 21.594 2.1580
+#            >>> st2=R152a(T=750, rho=100, eq=3)
+#            >>> print "%0.2f %0.5f" % (st2.h.kJkg-st.h.kJkg, st2.s.kJkgK-st.s.kJkgK)
+#            270.60 0.60934
+#            """, # Table III, Pag 117
             
         "R": 8.31451,
-        "cp": CP2,
+        "cp": Fi1,
+        "ref": "IIR", 
         
         "Tmin": Tt, "Tmax": 600.0, "Pmax": 100000.0, "rhomax": 18.1, 
         "Pmin": 0.064093, "rhomin": 18.031, 
@@ -150,9 +173,95 @@ class R152a(MEoS):
     helmholtz4 = {
         "__type__": "Helmholtz",
         "__name__": "Helmholtz equation of state for R-152a of Astina and Sato (2004)",
-        "__doc__":  u"""Astina, I.M. and Sato, H., "A Rigorous Thermodynamic Property Model for Fluid - Phase 1,1-Difluoroethane (R-152a)," Int. J. Thermophys., 25(6):1713-1733, 2004.""",
+        "__doi__": {"autor": "Astina, I.M. and Sato, H.",
+                    "title": "A Rigorous Thermodynamic Property Model for Fluid - Phase 1,1-Difluoroethane (R-152a)", 
+                    "ref": "Int. J. Thermophys., 25(6):1713-1733, 2004.",
+                    "doi": "10.1007/s10765-004-7731-8"}, 
+
+        "__test__": 
+            # Table III, Pag 1719 Saturation
+            """
+            >>> st=R152a(T=200, x=0.5, eq=4)
+            >>> print "%0.3f %0.5f %0.5g %0.5f %0.5g %0.5g %0.5g %0.5g %0.4g %0.4g %0.4g %0.4g %0.1f %0.1f" % (\
+                st.P.MPa, st.T, st.Liquido.rho, st.Gas.rho, st.Liquido.cv.kJkgK, st.Gas.cv.kJkgK, \
+                st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK, st.Liquido.w, st.Gas.w, \
+                st.Liquido.h.kJkg, st.Gas.h.kJkg, st.Liquido.s.kJkgK, st.Gas.s.kJkgK)
+            0.00608 200 1108.3 0.243503 1.02563 0.688696 1.53227 0.822899 1144.44 172.126 83.0708 452.53 0.504052 2.35135
+            >>> st=R152a(T=300, x=0.5, eq=4)
+            >>> print "%0.5f %i %0.2f %0.4f %0.5f %0.5f %0.5f %0.5f %0.3f %0.3f %0.3f %0.3f %0.5f %0.5f" % (\
+                st.P.MPa, st.T, st.Liquido.rho, st.Gas.rho, st.Liquido.cv.kJkgK, st.Gas.cv.kJkgK, \
+                st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK, st.Liquido.w, st.Gas.w, \
+                st.Liquido.h.kJkg, st.Gas.h.kJkg, st.Liquido.s.kJkgK, st.Gas.s.kJkgK)
+            0.62958 300 895.05 19.5363 1.14163 1.05165 1.80605 1.34292 635.933 184.955 246.936 523.189 1.16245 2.08329
+            >>> st=R152a(P=1e4, x=0.5, eq=4)
+            >>> print "%0.2f %0.5f %0.5g %0.5f %0.5g %0.5g %0.5g %0.5g %0.4g %0.4g %0.4g %0.4g %0.1f %0.1f" % (\
+                st.P.MPa, st.T, st.Liquido.rho, st.Gas.rho, st.Liquido.cv.kJkgK, st.Gas.cv.kJkgK, \
+                st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK, st.Liquido.w, st.Gas.w, \
+                st.Liquido.h.kJkg, st.Gas.h.kJkg, st.Liquido.s.kJkgK, st.Gas.s.kJkgK)
+            0.01 206.996 1095.05 0.387819 1.02888 0.709506 1.54024 0.846608 1106. 174.478 93.8198 457.773 0.556861 2.31513
+            >>> st=R152a(P=2e6, x=0.5, eq=4)
+            >>> print "%i %0.3f %0.3f %0.4f %0.5f %0.5f %0.5f %0.5f %0.3f %0.2f %0.3f %0.3f %0.4f %0.4f" % (\
+                st.P.MPa, st.T, st.Liquido.rho, st.Gas.rho, st.Liquido.cv.kJkgK, st.Gas.cv.kJkgK, \
+                st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK, st.Liquido.w, st.Gas.w, \
+                st.Liquido.h.kJkg, st.Gas.h.kJkg, st.Liquido.s.kJkgK, st.Gas.s.kJkgK)
+            2 345.817 755.354 67.2945 1.23824 1.25774 2.22690 1.99694 400.169 166.52 336.806 542.188 1.4355 2.0294
+            """
+            #Table , Pag 544
+            """
+            >>> st=R152a(T=200, P=1e4, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            0.01 200 1108.30 1.02563 1.53226 1144.46 83.0731 0.504046
+            >>> st=R152a(T=250, P=1e5, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            0.1 250 3.32533 0.85146 1.02093 185.395 490.101 2.17402
+            >>> st=R152a(T=300, P=5e5, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            0.5 300 14.9178 1.01270 1.25049 190.307 528.281 2.12554
+            >>> st=R152a(T=250, P=1e6, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            1 250 1011.4 1.06828 1.61778 887.901 162.066 0.852079
+            >>> st=R152a(T=450, P=2e6, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            2 450 39.2134 1.27176 1.47906 230.75 703.818 2.44002
+            >>> st=R152a(T=450, P=3e6, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            3 450 62.4333 1.29309 1.56254 221.167 692.526 2.37021
+            >>> st=R152a(T=300, P=5e6, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            5 300 910.768 1.14104 1.75988 681.439 247.753 1.14904
+            >>> st=R152a(T=350, P=1.5e7, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            15 350 829.854 1.22465 1.86556 577.309 339.639 1.39677
+            >>> st=R152a(T=400, P=2.5e7, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            25 400 764.468 1.30865 1.92116 526.849 433.427 1.61365
+            >>> st=R152a(T=250, P=4e7, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            40 250 1069.55 1.10283 1.5407 1076.73 183.511 0.788821
+            >>> st=R152a(T=300, P=4.5e7, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            45 300 999.404 1.175 1.61174 920.812 265.09 1.06788
+            >>> st=R152a(T=450, P=5e7, eq=4)
+            >>> print "%0.6g %0.5g %0.6g %0.6g %0.6g %0.6g %0.6g %0.6g" % (\
+                st.P.MPa, st.T, st.rho, st.cv.kJkgK, st.cp.kJkgK, st.w, st.h.kJkg, st.s.kJkgK)
+            50 450 782.051 1.40653 1.86403 612.067 528.382 1.76108
+            """, 
+
         "R": 8.314472,
-        "cp": CP1,
+        "cp": Fi2,
+        "ref": "IIR", 
+        "Tc": 386.41, "Pc": 4516,
         
         "Tmin": Tt, "Tmax": 450.0, "Pmax": 60000.0, "rhomax": 18.04 , 
         "Pmin": 0.064, "rhomin": 18.04 , 
@@ -160,7 +269,7 @@ class R152a(MEoS):
         "nr1": [1.753847317, -4.049760759, -2.277389257e-1, 7.087751950e-1,
                 -5.528619502e-1, -3.025046686e-2, 1.396289974e-1, 1.121238954e-4],
         "d1": [1, 1, 1, 2, 2, 3, 3, 4],
-        "t1": [0.5, 0.125, 2.875, 0.875, 1.875, 0.5, 1.875, 4],
+        "t1": [0.5, 1.125, 2.875, 0.875, 1.875, 0.5, 1.875, 4],
 
         "nr2": [1.181005890, 1.535785579, 7.468363045e-1, -1.252266405e-1,
                 -3.898223986e-2, -7.260588801e-2, -2.659302250e-3,
