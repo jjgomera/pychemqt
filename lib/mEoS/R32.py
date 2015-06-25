0a0b6dcd4dcdca83862207ed2ef21e9b078227d3
jjgomera <jjgomera@gmail.com>
2015-06-25 19:29:24 +0200

    Add test and documentation for Tillner-Roth for R32

--- lib/mEoS/R32.py
+++ lib/mEoS/R32.py
@@ -6,12 +6,7 @@ from lib import unidades
 
 
 class R32(MEoS):
-    """Multiparameter equation of state for R32
-
-    >>> r32=R32(T=300, P=0.1)
-    >>> print "%0.1f %0.5f %0.2f %0.3f %0.5f %0.4f %0.4f %0.2f" % (r32.T, r32.rho, r32.u.kJkg, r32.h.kJkg, r32.s.kJkgK, r32.cv.kJkgK, r32.cp.kJkgK, r32.w)
-    300.0 2.1109 514.38 561.75 2.6349 0.67906 0.84938 241.95
-    """
+    """Multiparameter equation of state for R32"""
     name = "difluoromethane"
     CASNumber = "75-10-5"
     formula = "CH2F2"
@@ -26,11 +21,11 @@ class R32(MEoS):
     momentoDipolar = unidades.DipoleMoment(1.978, "Debye")
     id = 645
 
-    CP1 = {"ao": 4.004486,
-           "an": [], "pow": [],
+    Fi1 = {"ao_log": [1, 3.004486],
+           "pow": [0, 1],
+           "ao_pow": [-8.258096, 6.353098],
            "ao_exp": [1.160761, 2.645151, 5.794987, 1.129475],
-           "exp": [798, 4185, 1806, 11510],
-           "ao_hyp": [], "hyp": []}
+           "titao": [798/Tc, 4185/Tc, 1806/Tc, 11510/Tc]}
 
     CP2 = {"ao": 36.79959/8.314471,
            "an": [-0.06304821/8.314471, 3.757936e-4/8.314471, -3.219812e-7/8.314471],
@@ -47,9 +42,181 @@ class R32(MEoS):
     helmholtz1 = {
         "__type__": "Helmholtz",
         "__name__": "Helmholtz equation of state for R-32 of Tillner-Roth & Yokozeki (1997)",
-        "__doc__":  u"""Tillner-Roth, R., Yokozeki, A. An international standard equation of state for difluoromethane (R-32) for temperatures from the triple point at 136.4 K to 435 K at pressures up to 70 MPa. J. Phys. Chem. Ref. Data 26 (1997), 1273 – 1328.""",
+        "__doi__": {"autor": "Tillner-Roth, R., Yokozeki, A.",
+                    "title": "An international standard equation of state for difluoromethane (R-32) for temperatures from the triple point at 136.4 K to 435 K at pressures up to 70 MPa", 
+                    "ref": "J. Phys. Chem. Ref. Data 26 (1997), 1273 – 1328.",
+                    "doi": "10.1063/1.556002"}, 
+
+        "__test__": 
+            #Table 12, Pag 1293
+            """
+            >>> st=R32(T=136.34, x=0.5)
+            >>> print "%0.2f %0.2f %0.5g %0.4f %0.2f %0.2f %0.2f %0.3f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            -136.81 0.05 1429.3 0.0022 -19.07 463.38 444.31 -0.104 3.2937 1.593 0.660
+            >>> st=R32(T=-120+273.15, x=0.5)
+            >>> print "%0.0f %0.2f %0.5g %0.4f %0.2f %0.2f %0.2f %0.3f %0.3f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            -120 0.48 1388.4 0.0195 7.52 447.81 455.33 0.079 3.003 1.573 0.674
+            >>> st=R32(T=-100+273.15, x=0.5)
+            >>> print "%0.0f %0.2f %0.5g %0.4f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            -100 3.81 1339 0.1385 38.83 429.48 468.31 0.2711 2.7515 1.560 0.703
+            >>> st=R32(T=-80+273.15, x=0.5)
+            >>> print "%0.0f %0.2f %0.5g %0.4f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            -80 18.65 1288.4 0.6129 90.02 410.71 480.73 0.4415 2.5679 1.561 0.754
+            >>> st=R32(T=-60+273.15, x=0.5)
+            >>> print "%0.0f %0.2f %0.5g %0.4f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            -60 64.96 1235.7 1.9690 101.38 390.73 492.11 0.5958 2.4290 1.576 0.833
+            >>> st=R32(T=-40+273.15, x=0.5)
+            >>> print "%0.0f %0.2f %0.5g %0.4f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            -40 177.41 1180.2 5.0651 133.23 368.79 502.02 0.7382 2.3200 1.608 0.940
+            >>> st=R32(T=-20+273.15, x=0.5)
+            >>> print "%0.0f %0.2f %0.5g %0.3f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            -20 405.75 1120.6 11.157 165.94 344.03 509.97 0.8720 2.2310 1.661 1.075
+            >>> st=R32(T=273.15, x=0.5)
+            >>> print "%0.0f %0.2f %0.5g %0.3f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            0 813.10 1055.3 22.091 200.00 315.30 515.30 1.0000 2.1543 1.745 1.251
+            >>> st=R32(T=20+273.15, x=0.5)
+            >>> print "%0.0f %0.1f %0.5g %0.3f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            20 1474.5 981.38 40.856 236.12 280.78 516.90 1.1253 2.0831 1.886 1.514
+            >>> st=R32(T=40+273.15, x=0.5)
+            >>> print "%0.0f %0.1f %0.5g %0.3f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            40 2478.3 893.04 73.268 275.61 237.09 512.71 1.2520 2.0091 2.163 2.001
+            >>> st=R32(T=60+273.15, x=0.5)
+            >>> print "%0.0f %0.1f %0.5g %0.2f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            60 3933.2 773.31 135.21 321.93 175.51 497.44 1.3898 1.9166 3.001 3.441
+            >>> st=R32(T=74+273.15, x=0.5)
+            >>> print "%0.0f %0.1f %0.5g %0.2f %0.2f %0.2f %0.2f %0.4f %0.4f %0.3f %0.3f" % (\
+                st.T.C, st.P.kPa, st.Liquido.rho, st.Gas.rho, st.Liquido.h.kJkg, st.Hvap.kJkg, st.Gas.h.kJkg, \
+                st.Liquido.s.kJkgK, st.Gas.s.kJkgK, st.Liquido.cp.kJkgK, st.Gas.cp.kJkgK)
+            74 5304.5 624.57 240.12 367.53 98.88 466.41 1.5179 1.8028 8.051 12.094
+            """
+            # Table 14, Pag 1297
+            """
+            >>> st=R32(T=-85+273.15, P=1e4)
+            >>> print "%0.0f %0.4f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            -85 0.3357 478.09 2.6526
+            >>> st=R32(T=-80+273.15, P=2e4)
+            >>> print "%0.0f %0.1f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            -80 1288.4 70.02 0.4415
+            >>> st=R32(T=273.15, P=3e4)
+            >>> print "%0.0f %0.4f %0.1f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            0 0.6907 541.3 2.7544
+            >>> st=R32(T=100+273.15, P=5e4)
+            >>> print "%0.0f %0.4f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            100 0.6721 628.13 2.9782
+            >>> st=R32(T=-55+273.15, P=1e5)
+            >>> print "%0.0f %0.1f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            -55 1222.2 109.29 0.6324
+            >>> st=R32(T=160+273.15, P=2e5)
+            >>> print "%0.0f %0.4f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            160 2.9073 686.47 2.8669
+            >>> st=R32(T=-30+273.15, P=3e5)
+            >>> print "%0.0f %0.1f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            -30 1151.0 149.46 0.8059
+            >>> st=R32(T=273.15, P=5e5)
+            >>> print "%0.0f %0.3f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            0 12.594 526.88 2.2651
+            >>> st=R32(T=-40+273.15, P=1e6)
+            >>> print "%0.0f %0.1f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            -40 1181.9 133.53 0.7365
+            >>> st=R32(T=30+273.15, P=2e6)
+            >>> print "%0.0f %0.2f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            30 940.17 255.28 1.1877
+            >>> st=R32(T=150+273.15, P=3e6)
+            >>> print "%0.0f %0.3f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            150 49.577 650.52 2.3649
+            >>> st=R32(T=-85+273.15, P=5e6)
+            >>> print "%0.0f %0.1f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            -85 1307.2 64.65 0.3932
+            >>> st=R32(T=100+273.15, P=1e7)
+            >>> print "%0.0f %0.2f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            100 566.69 416.83 1.6330
+            >>> st=R32(T=273.15, P=2e7)
+            >>> print "%0.0f %0.1f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            0 1112.9 204.05 0.9501
+            >>> st=R32(T=160+273.15, P=4e7)
+            >>> print "%0.0f %0.2f %0.2f %0.4f" % (st.T.C, st.rho, st.h.kJkg, st.s.kJkgK)
+            160 760.12 475.86 1.6700
+            """
+            # Table 15, Pag 1319
+            """
+            >>> st=R32(T=-20+273.15, P=5e5)
+            >>> print "%0.3f" % st.cp.kJkgK
+            1.660
+            >>> st=R32(T=155+273.15, P=2e4)
+            >>> print "%0.3f" % st.cp.kJkgK
+            1.033
+            >>> st=R32(T=5+273.15, P=1e6)
+            >>> print "%0.3f" % st.cp.kJkgK
+            1.773
+            >>> st=R32(T=80+273.15, P=6e6)
+            >>> print "%0.3f" % st.cp.kJkgK
+            61.802
+            >>> st=R32(T=65+273.15, P=1e7)
+            >>> print "%0.3f" % st.cp.kJkgK
+            2.140
+            """
+            # Table 16, Pag 1322
+            """
+            >>> st=R32(T=-20+273.15, P=5e5)
+            >>> print "%0.3f" % st.cv.kJkgK
+            0.934
+            >>> st=R32(T=155+273.15, P=2e4)
+            >>> print "%0.3f" % st.cv.kJkgK
+            0.873
+            >>> st=R32(T=5+273.15, P=1e6)
+            >>> print "%0.3f" % st.cv.kJkgK
+            0.941
+            >>> st=R32(T=80+273.15, P=6e6)
+            >>> print "%0.3f" % st.cv.kJkgK
+            1.411
+            >>> st=R32(T=65+273.15, P=1e7)
+            >>> print "%0.3f" % st.cv.kJkgK
+            0.993
+            """
+            # Table 17, Pag 1325
+            """
+            >>> st=R32(T=-20+273.15, P=5e5)
+            >>> print "%0.2f" % st.w
+            805.61
+            >>> st=R32(T=155+273.15, P=2e4)
+            >>> print "%0.2f" % st.w
+            284.42
+            >>> st=R32(T=5+273.15, P=1e6)
+            >>> print "%0.2f" % st.w
+            669.69
+            >>> st=R32(T=80+273.15, P=6e6)
+            >>> print "%0.2f" % st.w
+            150.04
+            >>> st=R32(T=65+273.15, P=1e7)
+            >>> print "%0.2f" % st.w
+            439.83
+            """,
+            
         "R": 8.314471,
-        "cp": CP1,
+        "cp": Fi1,
+        "ref": "IIR", 
         
         "Tmin": Tt, "Tmax": 435.0, "Pmax": 70000.0, "rhomax": 27.4734, 
         "Pmin": 0.480e-1, "rhomin": 27.4734, 
@@ -74,17 +241,18 @@ class R32(MEoS):
                     "title": "Equations of State for Technical Applications. III. Results for Polar Fluids", 
                     "ref": "Int. J. Thermophys., 24(1):111-162, 2003.",
                     "doi": "10.1023/A:1022362231796"}, 
-        "__test__": """
-            >>> st=R32(T=700, rho=200, eq=2)
-            >>> print "%0.4f %0.3f %0.4f" % (st.cp0.kJkgK, st.P.MPa, st.cp.kJkgK)
-            1.1421 30.358 1.8392
-            >>> st2=R32(T=750, rho=100, eq=2)
-            >>> print "%0.2f %0.5f" % (st2.h.kJkg-st.h.kJkg, st2.s.kJkgK-st.s.kJkgK)
-            235.85 0.59791
-            """, # Table III, Pag 117
+#        "__test__": """
+#            >>> st=R32(T=700, rho=200, eq=2)
+#            >>> print "%0.4f %0.3f %0.4f" % (st.cp0.kJkgK, st.P.MPa, st.cp.kJkgK)
+#            1.1421 30.358 1.8392
+#            >>> st2=R32(T=750, rho=100, eq=2)
+#            >>> print "%0.2f %0.5f" % (st2.h.kJkg-st.h.kJkg, st2.s.kJkgK-st.s.kJkgK)
+#            235.85 0.59791
+#            """, # Table III, Pag 117
             
         "R": 8.31451,
-        "cp": CP1,
+        "cp": Fi1,
+        "ref": "IIR", 
         
         "Tmin": Tt, "Tmax": 600.0, "Pmax": 100000.0, "rhomax": 27.41, 
         "Pmin": 0.047922, "rhomin": 27.41, 
@@ -158,7 +326,8 @@ class R32(MEoS):
                     "ref": "Fluid Phase Equilib., 222-223:107-118, 2004.",
                     "doi": "10.1016/j.fluid.2004.06.028"}, 
         "R": 8.314471,
-        "cp": CP1,
+        "cp": Fi1,
+        "ref": "IIR", 
 
         "Tmin": Tt, "Tmax": 620.0, "Pmax": 800000.0, "rhomax": 40., 
         "Pmin": 0.1, "rhomin": 40., 
@@ -207,7 +376,7 @@ class R32(MEoS):
                "db": [1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
                "cb": [0]*10,
 
-               "critical": 6,
+               "critical": 3,
                "gnu": 0.63, "gamma": 1.239, "R0": 1.03,
                "Xio": 0.194e-9, "gam0": 0.0496, "qd": 5.582925e-10, "Tcref": 526.8825}
 
