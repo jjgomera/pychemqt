56f2443c4e40991c74dd5228b124fac8b79b3d55
jjgomera <jjgomera@gmail.com>
2016-10-26 23:32:26 +0200

    tools/UI_unitConverter PEP8 adaptation, layout improve and code cleaning

--- tools/UI_unitConverter.py
+++ tools/UI_unitConverter.py
@@ -19,118 +19,120 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
 
 ###############################################################################
-# Tools with units converter
+# Tools with units converter UI
+#    - UI_conversorUnidades: Dialog to show all values of a magnitud in every
+#        supported unit
+#    - moneda: Customized unit converter for currency values
+#    - UI_unitConverter: Dialog to choose between the availables unit
 ###############################################################################
 
+
+import os
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from lib.unidades import Currency, getrates, _all
-from UI.delegate import CellEditor
 from lib.config import conf_dir
+from UI.delegate import CellEditor
 
 
 class UI_conversorUnidades(QtWidgets.QDialog):
+    """Dialog to show all values of a magnitud in every supported unit"""
     def __init__(self, unidad, valor=None, parent=None):
+        """
+        unidad: unidades class
+        valor: optional value to show
+        """
         super(UI_conversorUnidades, self).__init__(parent)
+        self.setWindowTitle(unidad.__title__)
+        self.mutex = QtCore.QMutex()
+
         self.unidad = unidad
-        self.magnitud = unidad.__name__
-        self.texto = unidad.__text__
-        self.unit = unidad.__units__
         if unidad.__tooltip__:
             self.tooltip = unidad.__tooltip__
         else:
             self.tooltip = unidad.__text__
         self.value = self.unidad(valor)
-        self.setWindowTitle(unidad.__title__)
-        self.gridLayout = QtWidgets.QGridLayout(self)
+
+        lyt = QtWidgets.QGridLayout(self)
         self.tabla = QtWidgets.QTableWidget()
-        self.tabla.setRowCount(len(self.texto))
+        self.tabla.setRowCount(len(unidad.__text__))
         self.tabla.setColumnCount(1)
         self.tabla.setItemDelegateForColumn(0, CellEditor(self))
         self.tabla.horizontalHeader().setVisible(False)
         self.tabla.horizontalHeader().setStretchLastSection(True)
-        if self.magnitud in ["SpecificVolume", "Density", "MassFlow", "VolFlow", "ThermalConductivity", "HeatTransfCoef"]:
-            self.resize(215, self.minimumHeight())
-        elif self.magnitud == "Currency":
-            self.resize(250, 500)
-        else:
-            self.resize(self.minimumSize())
+        self.tabla.setMaximumHeight(len(unidad.__text__)*24+4)
 
-        if self.magnitud in ["Temperature", "Area", "Volume",  "Length", "Angle", "Time"]:
-            x = 15
-        elif self.magnitud in["ThermalConductivity"]:
-            x = 10
-        elif self.magnitud in ["Speed", "Mass", "Acceleration", "Energy", "Enthalpy", "MassFlow", "Diffusivity", "Tension", "Solubility_parameter", "HeatTransfCoef"]:
-            x = 5
-        else:
-            x = 0
-        self.gridLayout.addItem(QtWidgets.QSpacerItem(x,15,QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed),2,0)
-        self.gridLayout.addItem(QtWidgets.QSpacerItem(x,15,QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed),2,2)
-
-        for i in range(len(self.texto)):
-            self.tabla.setVerticalHeaderItem(i, QtWidgets.QTableWidgetItem(self.texto[i]))
-            self.tabla.setRowHeight(i,24)
+        for i, txt in enumerate(unidad.__text__):
+            item = QtWidgets.QTableWidgetItem(txt)
+            self.tabla.setVerticalHeaderItem(i, item)
+            self.tabla.setRowHeight(i, 24)
             self.tabla.setItem(i, 0, QtWidgets.QTableWidgetItem(""))
-            self.tabla.item(i, 0).setTextAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignVCenter)
+            self.tabla.item(i, 0).setTextAlignment(
+                QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        for i, tip in enumerate(self.tooltip):
+            self.tabla.item(i, 0).setToolTip(tip)
+        self.tabla.cellChanged.connect(self.update)
+        lyt.addWidget(self.tabla, 2, 1)
 
-        for i in range(len(self.tooltip)):
-            self.tabla.item(i, 0).setToolTip(QtWidgets.QApplication.translate("pychemqt", self.tooltip[i]))
-
-        if valor:
-            self.rellenarTabla(self.value)
-            self.tabla.resizeColumnsToContents()
-        if self.magnitud!="Currency":
-            self.tabla.setFixedHeight(len(self.texto)*24+4)
-        self.gridLayout.addWidget(self.tabla, 2, 1, 1, 1)
-        self.tabla.cellChanged.connect(self.actualizar)
-
-        self.buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setCenterButtons(True)
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
-        self.gridLayout.addWidget(self.buttonBox,3,0,1,3)
+        lyt.addWidget(self.buttonBox, 3, 0, 1, 3)
 
-    def rellenarTabla(self, valor):
-        for i, key in enumerate(self.unit):
+        if valor:
+            self.fill(self.value)
+
+    def fill(self, valor):
+        for i, key in enumerate(self.unidad.__units__):
             self.tabla.item(i, 0).setText(valor.format(key))
 
-    def actualizar(self, fila, columna):
-        self.tabla.blockSignals(True)
-        self.value=self.unidad(float(self.tabla.item(fila, columna).text()), self.unit[fila])
-        self.rellenarTabla(self.value)
-        self.tabla.blockSignals(False)
+    def update(self, fila, columna):
+        if self.mutex.tryLock():
+            new = self.tabla.item(fila, columna).text()
+            self.value = self.unidad(float(new), self.unidad.__units__[fila])
+            self.fill(self.value)
+            self.mutex.unlock()
 
 
 class moneda(UI_conversorUnidades):
-
+    """Customized unit converter for currency values
+        - Add date and exchange update button at top
+        - Add country flags to easy recognize
+    """
     def __init__(self, valor=None, parent=None):
         super(moneda, self).__init__(Currency, valor=valor, parent=parent)
 
         self.fecha = QtWidgets.QLabel(QtWidgets.QApplication.translate(
-            "pychemqt", "Date::") + self.value.fecha)
-        self.gridLayout.addWidget(self.fecha, 0, 1)
+            "pychemqt", "Date") + ": " + self.value.fecha)
+        self.layout().addWidget(self.fecha, 0, 1)
         self.botonActualizar = QtWidgets.QPushButton(
             QtWidgets.QApplication.translate("pychemqt", "Update"))
         self.botonActualizar.clicked.connect(self.getrates)
-        self.gridLayout.addWidget(self.botonActualizar, 1, 1)
+        self.layout().addWidget(self.botonActualizar, 1, 1)
 
         for i in range(len(Currency.__units__)):
             self.tabla.verticalHeaderItem(i).setIcon(QtGui.QIcon(QtGui.QPixmap(
-                os.environ["pychemqt"]+"/images/flag/%s.gif" % Currency.__units__[i])))
+                os.environ["pychemqt"] +
+                "/images/flag/%s.gif" % Currency.__units__[i])))
 
     def getrates(self):
-        filename = conf_dir+"moneda.dat"
+        filename = conf_dir + "moneda.dat"
         getrates(filename)
         self.value = self.unidad(self.value)
-        self.fecha.setText(QtWidgets.QApplication.translate("pychemqt", "Date:") +
-                           self.value.fecha)
+        self.fecha.setText(QtWidgets.QApplication.translate(
+            "pychemqt", "Date") + ": " + self.value.fecha)
         if self.value != 0:
-            self.actualizar(0, 0)
-
+            self.update(0, 0)
 
 
 class UI_unitConverter(QtWidgets.QDialog):
+    """Dialog to choose between the availables unit,
+    used as main window tool
+    """
+
     def __init__(self, parent=None):
         super(UI_unitConverter, self).__init__(parent)
         self.setWindowTitle(
@@ -157,6 +159,7 @@ class UI_unitConverter(QtWidgets.QDialog):
         else:
             dialog = UI_conversorUnidades(_all[indice])
         dialog.exec_()
+
 
 if __name__ == "__main__":
     import sys
