7959be7e50395208d2725f6d0c3e51e6312ebe76
jjgomera <jjgomera@gmail.com>
2017-02-13 11:06:05 +0100

    Fix bug #13 about scintilla optional dependence

--- tools/codeEditor.py
+++ tools/codeEditor.py
@@ -18,7 +18,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
 
-
 ###############################################################################
 # qsci_simple_pythoneditor.pyw
 #
@@ -27,81 +26,94 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
 #
-# For now no integrated in pychemqt
-# Possible use to let user define custom functionality, define custom
+# For now only integrated in pychemqt in meos tools for view custom method
+# Other possible use to let user define custom functionality, define custom
 # equipment, units, mEoS...
 ###############################################################################
 
-import sys
-from PyQt5 import QtGui, QtWidgets
 
+import os
+import sys
+
+from PyQt5 import QtGui, QtWidgets
 try:
     from PyQt5.Qsci import QsciScintilla, QsciLexerPython
 except:
     pass
 
 
-class SimplePythonEditor(QsciScintilla):
-    ARROW_MARKER_NUM = 8
+if os.environ["PyQt5.Qsci"] == "True":
+    # With scintilla available use as python editor
+    class SimplePythonEditor(QsciScintilla):
+        ARROW_MARKER_NUM = 8
 
-    def __init__(self, parent=None):
-        super(SimplePythonEditor, self).__init__(parent)
+        def __init__(self, parent=None):
+            super(SimplePythonEditor, self).__init__(parent)
 
-        # Set the default font
-        font = QtGui.QFont()
-        font.setFamily('Courier')
-        font.setFixedPitch(True)
-        font.setPointSize(10)
-        self.setFont(font)
-        self.setMarginsFont(font)
+            # Set the default font
+            font = QtGui.QFont()
+            font.setFamily('Courier')
+            font.setFixedPitch(True)
+            font.setPointSize(10)
+            self.setFont(font)
+            self.setMarginsFont(font)
 
-        # Margin 0 is used for line numbers
-        fontmetrics = QtGui.QFontMetrics(font)
-        self.setMarginsFont(font)
-        self.setMarginWidth(0, fontmetrics.width("000") + 6)
-        self.setMarginLineNumbers(0, True)
-        self.setMarginsBackgroundColor(QtGui.QColor("#cccccc"))
+            # Margin 0 is used for line numbers
+            fontmetrics = QtGui.QFontMetrics(font)
+            self.setMarginsFont(font)
+            self.setMarginWidth(0, fontmetrics.width("000") + 6)
+            self.setMarginLineNumbers(0, True)
+            self.setMarginsBackgroundColor(QtGui.QColor("#cccccc"))
 
-        # Clickable margin 1 for showing markers
-        self.setMarginSensitivity(1, True)
-        self.marginClicked.connect(self.on_margin_clicked)
-        self.markerDefine(QsciScintilla.RightArrow, self.ARROW_MARKER_NUM)
-        self.setMarkerBackgroundColor(
-            QtGui.QColor("#ee1111"), self.ARROW_MARKER_NUM)
+            # Clickable margin 1 for showing markers
+            self.setMarginSensitivity(1, True)
+            self.marginClicked.connect(self.on_margin_clicked)
+            self.markerDefine(QsciScintilla.RightArrow, self.ARROW_MARKER_NUM)
+            self.setMarkerBackgroundColor(
+                QtGui.QColor("#ee1111"), self.ARROW_MARKER_NUM)
 
-        # Brace matching: enable for a brace immediately before or after
-        # the current position
-        #
-        self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
+            # Brace matching: enable for a brace immediately before or after
+            # the current position
+            #
+            self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
 
-        # Current line visible with special background color
-        self.setCaretLineVisible(True)
-        self.setCaretLineBackgroundColor(QtGui.QColor("#ffe4e4"))
+            # Current line visible with special background color
+            self.setCaretLineVisible(True)
+            self.setCaretLineBackgroundColor(QtGui.QColor("#ffe4e4"))
 
-        # Set Python lexer
-        # Set style for Python comments (style number 1) to a fixed-width
-        # courier.
-        #
-        lexer = QsciLexerPython()
-        lexer.setDefaultFont(font)
-        self.setLexer(lexer)
-        # self.SendScintilla(QsciScintilla.SCI_STYLESETFONT, 1, 'Courier')
-        # self.SendScintilla(QsciScintilla.SCI_STYLESETFONT, 1)
+            # Set Python lexer
+            # Set style for Python comments (style number 1) to a fixed-width
+            # courier.
+            #
+            lexer = QsciLexerPython()
+            lexer.setDefaultFont(font)
+            self.setLexer(lexer)
+            # self.SendScintilla(QsciScintilla.SCI_STYLESETFONT, 1, 'Courier')
+            # self.SendScintilla(QsciScintilla.SCI_STYLESETFONT, 1)
 
-        # Don't want to see the horizontal scrollbar at all
-        # Use raw message to Scintilla here (all messages are documented
-        # here: http://www.scintilla.org/ScintillaDoc.html)
-        self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
+            # Don't want to see the horizontal scrollbar at all
+            # Use raw message to Scintilla here (all messages are documented
+            # here: http://www.scintilla.org/ScintillaDoc.html)
+            self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
 
-        # not too small
-        # self.setMinimumSize(700, 450)
+            # not too small
+            # self.setMinimumSize(700, 450)
 
-    def on_margin_clicked(self, nmargin, nline, modifiers):
-        # Toggle marker for the line the margin was clicked on
-        if self.markersAtLine(nline) != 0:
-            self.markerDelete(nline, self.ARROW_MARKER_NUM)
-        else:
-            self.markerAdd(nline, self.ARROW_MARKER_NUM)
+        def on_margin_clicked(self, nmargin, nline, modifiers):
+            # Toggle marker for the line the margin was clicked on
+            if self.markersAtLine(nline) != 0:
+                self.markerDelete(nline, self.ARROW_MARKER_NUM)
+            else:
+                self.markerAdd(nline, self.ARROW_MARKER_NUM)
+
+else:
+    # Use a normal Qt widget
+    class SimplePythonEditor(QtWidgets.QPlainTextEdit):
+        """Simple text editor without scintilla dependence"""
+        def setText(self, txt):
+            """Define the same functionality as for scintilla api"""
+            self.setPlainText(txt)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
