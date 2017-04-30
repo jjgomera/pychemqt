b0d617f3b84f5838beae5d338cf2e51b0b38d0a2
jjgomera <jjgomera@gmail.com>
2017-04-30 20:28:54 +0200

    Update date of copyright to 2007

--- pychemqt.py
+++ pychemqt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-"""pychemqt, Chemical Engineering Process simulator
-Copyright (C) 2016, Juan José Gómez Romera <jjgomera@gmail.com>
+"""Pychemqt, Chemical Engineering Process simulator
+Copyright (C) 2009-2017, Juan José Gómez Romera <jjgomera@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
