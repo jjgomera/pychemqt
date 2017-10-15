f86a07567076dc9651e3e3fc5a7491259c252871
jjgomera <jjgomera@gmail.com>
2017-10-15 19:49:55 +0200

    Fix travis integration #10

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# Initialization of environment
+import os
+os.environ["pychemqt"] = "/home/travis/build/jjgomera/pychemqt/"
+os.environ["freesteam"] = "True"
+os.environ["pybel"] = "True"
+os.environ["CoolProp"] = "True"
+os.environ["refprop"] = "True"
+os.environ["ezodf"] = "True"
+os.environ["openpyxl"] = "True"
+os.environ["xlwt"] = "False"
+os.environ["icu"] = "False"
+os.environ["reportlab"] = "False"
+os.environ["PyQt5.Qsci"] = "True"
+
+
 # Main script for unit testing
 
 from unittest import TextTestRunner, TestSuite
