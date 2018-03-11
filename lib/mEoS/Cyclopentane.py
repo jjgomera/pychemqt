9925e0f63a54eef8350691986fb282d9d15b72e6
jjgomera <jjgomera@gmail.com>
2018-03-11 21:14:47 +0100

    Add testing for Gedanitz mEoS for Cyclopentane

--- lib/mEoS/Cyclopentane.py
+++ lib/mEoS/Cyclopentane.py
@@ -18,6 +18,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
 
+from unittest import TestCase
+
 from lib.meos import MEoS
 from lib import unidades
 
@@ -45,43 +47,17 @@ class Cyclopentane(MEoS):
            "titao": [230/Tc, 1180/Tc, 2200/Tc, 5200/Tc],
            "ao_hyp": [], "hyp": []}
 
-    CP1 = {"ao": 3.263,
-           "an": [], "pow": [],
-           "ao_exp": [2.151, 19.55, 14.45, 3.594],
-           "exp": [179.0, 1336.0, 2911.0, 6420.0],
-           "ao_hyp": [], "hyp": []}
-
     gedanitz = {
         "__type__": "Helmholtz",
-        "__name__": "Helmholtz equation of state for cyclopentane of Gedanitz et al. (2015).",
+        "__name__": "Helmholtz equation of state for cyclopentane of Gedanitz "
+                    "et al. (2015)",
         "__doi__": {"autor": "Gedanitz, H., Davila, M.J., Lemmon, E.W.",
-                    "title": "Speed of Sound Measurements and a Fundamental Equation of State for Cyclopentane",
-                    "ref": "J. Chem. Eng. Data, 2015, 60 (5), pp 1331–1337",
+                    "title": "Speed of Sound Measurements and a Fundamental "
+                             "Equation of State for Cyclopentane",
+                    "ref": "J. Chem. Eng. Data, 60(5) (2015) 1331-1337",
                     "doi": "10.1021/je5010164"},
-        "__test__": """
-            >>> st=Cyclopentane(T=330, rhom=0.01)
-            >>> print "%0.0f %0.7g %0.2f %0.7g %0.7g %0.7g" % (\
-                st.T, st.P.MPa, st.rhoM, st.cvM.JmolK, st.cpM.JmolK, st.w)
-            330 0.02720379 0.01 86.25843 94.88857 205.6768
-            >>> st=Cyclopentane(T=330, rhom=11)
-            >>> print "%0.0f %0.7g %0.1f %0.7g %0.7g %0.7g" % (\
-                st.T, st.P.MPa, st.rhoM, st.cvM.JmolK, st.cpM.JmolK, st.w)
-            330 75.55974 11.0 100.6003 130.5568 1471.842
-            >>> st=Cyclopentane(T=530, rhom=1)
-            >>> print "%0.0f %0.7g %0.1f %0.7g %0.7g %0.7g" % (\
-                st.T, st.P.MPa, st.rhoM, st.cvM.JmolK, st.cpM.JmolK, st.w)
-            530 3.240235 1.0 156.4924 187.7243 195.4293
-            >>> st=Cyclopentane(T=512, rhom=4)
-            >>> print "%0.0f %0.7g %0.1f %0.7g %0.7g %0.7g" % (\
-                st.T, st.P.MPa, st.rhoM, st.cvM.JmolK, st.cpM.JmolK, st.w)
-            512 4.601539 4.0 161.5786 22857.91 113.0171
-            >>> st=Cyclopentane(T=520, rhom=6)
-            >>> print "%0.0f %0.7g %0.1f %0.7g %0.7g %0.7g" % (\
-                st.T, st.P.MPa, st.rhoM, st.cvM.JmolK, st.cpM.JmolK, st.w)
-            520 6.522373 6.0 159.2304 276.7530 234.2660
-            """, # Table 5, Pag F
 
-        "R": 8.314472, "rhoc": 3.92,
+        "R": 8.3144621,
         "cp": Fi1,
         "ref": "NBP",
 
@@ -106,39 +82,7 @@ class Cyclopentane(MEoS):
         "gamma3": [1.22, 0.32, 0.22, 1.94, 1.21],
         "epsilon3": [0.684, 0.7, 0.77, 0.625, 0.42]}
 
-    lemmon = {
-        "__type__": "Helmholtz",
-        "__name__": "Helmholtz equation of state for cyclopentane of Lemmon et al. (2008).",
-        "__doi__": {"autor": "Lemmon, E.W.",
-                    "title": "",
-                    "ref": "unpublished equation",
-                    "doi": ""},
-        "R": 8.314472,
-        "cp": CP1,
-        "ref": "NBP",
-
-        "Tmin": Tt, "Tmax": 600.0, "Pmax": 200000.0, "rhomax": 12.2,
-        "Pmin": 0.0089, "rhomin": 12.1,
-
-        "nr1": [0.4909331e-1, 0.1244679e1, -0.1990222e1, -0.5245596, 0.1764215],
-        "d1": [4, 1, 1, 2, 3],
-        "t1": [1, 0.23, 0.94, 1.08, 0.53],
-
-        "nr2": [-0.1066798e1, -0.5028152, 0.8484762, -0.4547443, -0.2767817e-1],
-        "d2": [1, 3, 2, 2, 7],
-        "t2": [1.67, 1.8, 1.3, 2.5, 1.0],
-        "c2": [2, 2, 1, 2, 1],
-        "gamma2": [1]*5,
-
-        "nr3": [0.9455318, -0.3014822, -0.1675668, -0.637707],
-        "d3": [1, 1, 3, 3],
-        "t3": [0.87, 1.4, 2.4, 1.3],
-        "alfa3": [1.023, 1.383, 0.996, 7.038],
-        "beta3": [1.7, 1.55, 1.07, 87.17],
-        "gamma3": [1.1, 0.64, 0.5, 1.26],
-        "epsilon3": [0.713, 0.917, 0.688, 0.748]}
-
-    eq = gedanitz, lemmon
+    eq = gedanitz,
 
     _vapor_Pressure = {
         "eq": 5,
@@ -152,3 +96,38 @@ class Cyclopentane(MEoS):
         "eq": 3,
         "ao": [-0.0559, -6.4211, -46.926, 28.082, -70.838],
         "exp": [0.1, 0.65, 3.2, 3.55, 7.5]}
+
+
+class Test(TestCase):
+
+    def test_gedanitz(self):
+        # Table 5, Pag 1336
+        st = Cyclopentane(T=330, rhom=0.01)
+        self.assertEqual(round(st.P.MPa, 8), 0.02720379)
+        self.assertEqual(round(st.cvM.JmolK, 5), 86.25843)
+        self.assertEqual(round(st.cpM.JmolK, 5), 94.88857)
+        self.assertEqual(round(st.w, 4), 205.6768)
+
+        st = Cyclopentane(T=330, rhom=11)
+        self.assertEqual(round(st.P.MPa, 5), 75.55974)
+        self.assertEqual(round(st.cvM.JmolK, 4), 100.6003)
+        self.assertEqual(round(st.cpM.JmolK, 4), 130.5568)
+        self.assertEqual(round(st.w, 3), 1471.842)
+
+        st = Cyclopentane(T=530, rhom=1)
+        self.assertEqual(round(st.P.MPa, 6), 3.240235)
+        self.assertEqual(round(st.cvM.JmolK, 4), 156.4924)
+        self.assertEqual(round(st.cpM.JmolK, 4), 187.7243)
+        self.assertEqual(round(st.w, 4), 195.4293)
+
+        st = Cyclopentane(T=512, rhom=4)
+        self.assertEqual(round(st.P.MPa, 6), 4.601539)
+        self.assertEqual(round(st.cvM.JmolK, 4), 161.5786)
+        self.assertEqual(round(st.cpM.JmolK, 2), 22857.91)
+        self.assertEqual(round(st.w, 4), 113.0171)
+
+        st = Cyclopentane(T=520, rhom=6)
+        self.assertEqual(round(st.P.MPa, 6), 6.522373)
+        self.assertEqual(round(st.cvM.JmolK, 4), 159.2304)
+        self.assertEqual(round(st.cpM.JmolK, 4), 276.7530)
+        self.assertEqual(round(st.w, 4), 234.2660)
