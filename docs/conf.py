cd11ebee10a0fca11127c3c7642fe63d1ff29194
jjgomera <jjgomera@gmail.com>
2018-09-27 23:01:21 +0200

    Fix relative path for documentation building #28

--- docs/conf.py
+++ docs/conf.py
@@ -23,10 +23,8 @@ import sys
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.append(os.path.join(os.path.abspath(os.pardir)))
-sys.path.insert(0, os.path.abspath('../lib'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # Define pychemqt environment
 os.environ["pychemqt"] = os.path.abspath('../')
