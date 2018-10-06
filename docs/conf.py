72a2acdb1c41acbeb0b7abe29a1e2a7aa55d2f13
jjgomera <jjgomera@gmail.com>
2018-10-06 16:18:14 +0200

    Change documentation theme to readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -205,16 +205,13 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-        "stickysidebar": True,
-        # "sidebarbgcolor": "#683d32",
-        # "footerbgcolor": "#683d32",
-        # "relbarbgcolor": "#683d32"
+        "display_version": False,
         }
 
 # Add any paths that contain custom themes here, relative to this directory.
