8eec049e784c92ad18cc92b48e546f27c2c917cd
jjgomera <jjgomera@gmail.com>
2019-09-16 20:51:35 +0200

    Add cost index from sep-2019 and oct-2019 issues of chemical engineering magazine

--- docs/conf.py
+++ docs/conf.py
@@ -124,7 +124,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
+    # 'sphinx.ext.napoleon',
     'numpydoc',
 ]
 
