334a2fdfec8c9f4cd75a002d719b5880fe92e61a
jjgomera <jjgomera@gmail.com>
2022-06-22 21:59:50 +0200

    Add Barati drag correlation

--- lib/friction.py
+++ lib/friction.py
@@ -64,7 +64,7 @@ TODO
 '''
 
 
-from math import exp, log, log10, sqrt, sin, pi
+from math import exp, log, log10, sqrt, sin, pi, tanh
 
 from scipy.optimize import fsolve
 
@@ -235,6 +235,13 @@ __doi__ = {
          "title": "Chemical Engineering Fluid Mechanics, 3rd Edition",
          "ref": "CRC Press, 2017",
          "doi": ""},
+    30:
+        {"autor": "Barati, R., Neyshabouri, S.A.A.S, Ahmadi, G.",
+         "title": "Development of Empirical Models with High Accuracy for "
+                  "Estimation of Drag Coefficient of Flow around a Smooth "
+                  "Sphere: An Evolutionary Approach",
+         "ref": "Powder Technology 257 (2014) 11-19",
+         "doi": "10.1016/j.powtec.2014.02.045"},
 
     # 30:
         # {"autor": "",
@@ -1394,6 +1401,67 @@ def Ft(D):
     else:
         ft = 0.012
     return ft
+
+
+@refDoc(__doi__, [30])
+def Barati(Re, extended=False):
+    r'''Calculates drag coefficient of a smooth sphere using the method in
+    [1]_.
+
+    For Re < 2e5
+
+    .. math::
+        C_D = 5.4856\times10^9\tanh(4.3774\times10^{-9}/Re)
+        + 0.0709\tanh(700.6574/Re) + 0.3894\tanh(74.1539/Re)
+        - 0.1198\tanh(7429.0843/Re) + 1.7174\tanh[9.9851/(Re+2.3384)] + 0.4744
+
+    For 2e5 <= Re < 1e6
+
+    .. math::
+        C_D = 8x10^{-6}\left[(Re/6530)^2 + \tanh(Re) - 8\ln(Re)/\ln(10)\right]
+        - 0.4119\exp(-2.08x10^{43}/[Re + Re^2]^4)
+        - 2.1344\exp(-\left{\left[\ln(Re^2 + 10.7563)/\ln(10)\right]^2
+        + 9.9867\right}/Re)
+        + 0.1357\exp(-[(Re/1620)^2 + 10370]/Re)
+        - 8.5\times 10^{-3}\{2\ln[\tanh(\tanh(Re))]/\ln(10) - 2825.7162\}/Re
+
+    Parameters
+    ----------
+    Re : float
+        Reynolds number of the sphere, [-]
+    extended : bool
+        Use the extended version on all Re range of equation
+
+    Returns
+    -------
+    Cd : float
+        Drag coefficient [-]
+
+    Examples
+    --------
+    Selected values from Table 6 in [1]_.
+
+    >>> print("%0.2f" % Barati(0.002))
+    12008.86
+    >>> print("%0.2f" % Barati(0.002, extended=True))
+    12034.71
+    >>> print("%0.2f" % Barati(1000))
+    0.47
+    '''
+
+    if not extended and Re < 2e5:
+        # Eq 22
+        Cd = 5.4856e9*tanh(4.3774e-9/Re) + 0.0709*tanh(700.6574/Re) \
+            + 0.3894*tanh(74.1539/Re) - 0.1198*tanh(7429.0843/Re) \
+            + 1.7174*tanh(9.9851/(Re+2.3384)) + 0.4744
+    elif Re <= 1e6:
+        # Eq 23
+        Cd = 8e-6*((Re/6530)**2 + tanh(Re) - 8*log(Re)/log(10)) \
+            - 0.4119*exp(-2.08e43/(Re+Re**2)**4) \
+            - 2.1344*exp(-((log(Re**2 + 10.7563)/log(10))**2 + 9.9867)/Re) \
+            + 0.1357*exp(-((Re/1620)**2 + 10370)/Re) \
+            - 8.5e-3*(2*log(tanh(tanh(Re)))/log(10) - 2825.7162)/Re + 2.4795
+    return Cd
 
 
 if __name__ == "__main__":
