6ee53c2524cc58852666c35524295d6bfb7a6702
Juan José Gómez Romera <jjgomera@gmail.com>
2022-12-12 18:39:56 +0100

    Add Thol mEoS for MDM and MD2M

--- lib/mEoS/MD2M.py
+++ lib/mEoS/MD2M.py
@@ -18,6 +18,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
 
+from unittest import TestCase
+
 from lib import unidades
 from lib.meos import MEoS
 
@@ -30,20 +32,67 @@ class MD2M(MEoS):
     synonym = "MD2M"
     _refPropName = "MD2M"
     _coolPropName = "MD2M"
-    rhoc = unidades.Density(284.1716396703609)
-    Tc = unidades.Temperature(599.40)
-    Pc = unidades.Pressure(1227.0, "kPa")
-    M = 310.685  # g/mol
+    rhoc = unidades.Density(268.4321856)
+    Tc = unidades.Temperature(599.4)
+    Pc = unidades.Pressure(1144.0, "kPa")
+    M = 310.6854  # g/mol
     Tt = unidades.Temperature(205.2)
-    Tb = unidades.Temperature(467.51)
-    f_acent = 0.668
+    Tb = unidades.Temperature(467.59)
+    f_acent = 0.635
     momentoDipolar = unidades.DipoleMoment(1.12, "Debye")
     # id = 1837
+
+    Fi1 = {"ao_log": [1, 3],
+           "pow": [0, 1],
+           "ao_pow": [131.089725, -26.3839138],
+           "ao_exp": [28.59, 56.42, 50.12],
+           "titao": [20/Tc, 1180/Tc, 4240/Tc]}
+
+    CP2 = {"ao": 4,
+           "ao_exp": [28.59, 56.42, 50.12],
+           "exp": [20, 1180, 4240]}
 
     f = 8.314472
     CP1 = {"ao": 331.9/f,
            "ao_sinh": [777.100/f], "sinh": [1813.8],
            "ao_cosh": [521.400/f], "cosh": [795.1]}
+
+    thol = {
+        "__type__": "Helmholtz",
+        "__name__": "Helmholtz equation of state for "
+                    "decamethyltetrasiloxane of Thol (2017).",
+        "__doi__": {
+            "autor": "Thol, M., Dubberke, F.H., Baumhögger, E., Vrabec, J., "
+                     "Span, R.",
+            "title": "Speed of Sound Measuements and Fundamental Equations "
+                     "State for Octamethyltrisiloxane and "
+                     "Decamethyltetrasiloxane",
+            "ref": "J. Chem. Eng. Data 62(9) (2017) 2633-2648",
+            "doi": "10.1021/acs.jced.7b00092"},
+
+        "R": 8.3144598,
+        "cp": CP2,
+        "ref": "NBP",
+
+        "Tmin": Tt, "Tmax": 600, "Pmax": 130000,
+
+        "nr1": [0.01458333, 3.227554, -3.503565, -2.017391, 0.8606129],
+        "d1": [4, 1, 1, 2, 3],
+        "t1": [1, 0.319, 0.829, 0.78, 0.687],
+
+        "nr2": [-2.196015, -0.9289014, 2.027740, -0.9168439, -0.06383507],
+        "d2": [1, 3, 2, 2, 7],
+        "t2": [1.29, 3.91, 0.77, 3.055, 1.013],
+        "c2": [2, 2, 1, 2, 1],
+        "gamma2": [1]*5,
+
+        "nr3": [2.674255, 0.04662529, -0.3835361, -0.4273462, -1.148009],
+        "d3": [1, 1, 3, 2, 2],
+        "t3": [1.07, 1.89, 1.133, 0.826, 0.83],
+        "alfa3": [0.982, 2.7, 1.347, 0.864, 1.149],
+        "beta3": [0.7323, 0.543, 1.26, 0.878, 2.22],
+        "gamma3": [1.042, 1.1, 1.146, 1.085, 0.6844],
+        "epsilon3": [0.874, 1.43, 0.855, 0.815, 0.491]}
 
     colonna = {
         "__type__": "Helmholtz",
@@ -53,7 +102,7 @@ class MD2M(MEoS):
                              " [(CH3)3-Si-O1/2]2-[O-Si-(CH3)2]i=1,…,3, and "
                              "[O-Si-(CH3)2]6",
                     "ref": "Fluid Phase Equilibria 263:115-130, 2008",
-                    "doi":  "10.1016/j.fluid.2007.10.001"},
+                    "doi": "10.1016/j.fluid.2007.10.001"},
 
         "R": 8.314472,
         "cp": CP1,
@@ -73,7 +122,7 @@ class MD2M(MEoS):
         "c2": [1, 1, 2, 2, 3, 3],
         "gamma2": [1]*6}
 
-    eq = colonna,
+    eq = thol, colonna
 
     _surface = {
         "__doi__": {
@@ -86,14 +135,59 @@ class MD2M(MEoS):
 
     _vapor_Pressure = {
         "eq": 3,
-        "n": [-0.10029e2, 0.44434e1, -0.36753e1, -0.68925e1, -0.32211e1],
-        "t": [1.0, 1.5, 2.06, 3.5, 10.0]}
+        "n": [-10.174, 9.607, -10.08, -7.242, -30.56],
+        "t": [1, 1.5, 1.83, 4.15, 17.8]}
     _liquid_Density = {
         "eq": 1,
-        "n": [0.12608e2, -0.32120e2, 0.33559e2, -0.11695e2, 0.76192],
-        "t": [0.48, 0.64, 0.8, 1.0, 2.6]}
+        "n": [8.215, -24.65, 47.23, -42.44, 15.18],
+        "t": [0.498, 0.855, 1.22, 1.6, 2.04]}
     _vapor_Density = {
         "eq": 2,
-        "n": [-0.24684e1, -0.71262e1, -0.27601e2, -0.49458e2, -0.24106e2,
-              -0.19370e3],
-        "t": [0.376, 0.94, 2.9, 5.9, 6.2, 13.0]}
+        "n": [-4.5483, -101.989, 224.06, -182.79, -110.45, -330.87],
+        "t": [0.428, 2.32, 2.8, 3.3, 8.5, 17.5]}
+
+
+class Test(TestCase):
+
+    # Reference state fail with last decimal number
+    def test_thol(self):
+        # Table 13, Pag. 2646
+        st = MD2M(T=350, rhom=0.0001)
+        self.assertEqual(round(st.P.MPa, 10), 0.0002908572)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 467.6281)
+        self.assertEqual(round(st.w, 5), 97.60521)
+        self.assertEqual(round(st.hM.Jmol, 2), -19315.07)
+        # self.assertEqual(round(st.sM.JmolK, 5), -12.27574)
+        self.assertEqual(round(st.aM.Jmol, 2), -17927.15)
+
+        st = MD2M(T=350, rhom=2.9)
+        self.assertEqual(round(st.P.MPa, 5), 97.64338)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 568.1542)
+        self.assertEqual(round(st.w, 3), 1295.433)
+        # self.assertEqual(round(st.hM.Jmol, 2), -47644.93)
+        self.assertEqual(round(st.sM.JmolK, 4), -208.0510)
+        # self.assertEqual(round(st.aM.Jmol, 3), -8497.211)
+
+        st = MD2M(T=500, rhom=0.05)
+        self.assertEqual(round(st.P.MPa, 7), 0.1848863)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 587.2108)
+        self.assertEqual(round(st.w, 4), 103.4085)
+        # self.assertEqual(round(st.hM.Jmol, 2), 58103.75)
+        self.assertEqual(round(st.sM.JmolK, 4), 118.4514)
+        # self.assertEqual(round(st.aM.Jmol, 3), -4819.665)
+
+        st = MD2M(T=500, rhom=2.7)
+        self.assertEqual(round(st.P.MPa, 4), 115.7619)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 649.4756)
+        self.assertEqual(round(st.w, 3), 1168.896)
+        self.assertEqual(round(st.hM.Jmol, 2), 48958.28)
+        # self.assertEqual(round(st.sM.JmolK, 6), 5.385695)
+        self.assertEqual(round(st.aM.Jmol, 3), 3390.654)
+
+        st = MD2M(T=590, rhom=2.5)
+        self.assertEqual(round(st.P.MPa, 5), 89.78835)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 685.6283)
+        self.assertEqual(round(st.w, 4), 987.7431)
+        # self.assertEqual(round(st.hM.Jmol, 1), 101922.4)
+        # self.assertEqual(round(st.sM.JmolK, 4), 121.0574)
+        # self.assertEqual(round(st.aM.Jmol, 3), -5416.819)
--- lib/mEoS/MDM.py
+++ lib/mEoS/MDM.py
@@ -18,6 +18,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
 
+from unittest import TestCase
+
 from lib import unidades
 from lib.meos import MEoS
 
@@ -30,20 +32,67 @@ class MDM(MEoS):
     synonym = "MDM"
     _refPropName = "MDM"
     _coolPropName = "MDM"
-    rhoc = unidades.Density(256.73940949935815)
-    Tc = unidades.Temperature(564.09)
-    Pc = unidades.Pressure(1415.0, "kPa")
-    M = 236.531  # g/mol
+    rhoc = unidades.Density(268.22667564)
+    Tc = unidades.Temperature(565.3609)
+    Pc = unidades.Pressure(1437.5, "kPa")
+    M = 236.53146  # g/mol
     Tt = unidades.Temperature(187.2)
-    Tb = unidades.Temperature(425.66)
-    f_acent = 0.529
+    Tb = unidades.Temperature(425.63)
+    f_acent = 0.524
     momentoDipolar = unidades.DipoleMoment(1.079, "Debye")
     # id = 1893
+
+    Fi1 = {"ao_log": [1, 3],
+           "pow": [0, 1],
+           "ao_pow": [117.9946064218, -19.6600754238],
+           "ao_exp": [28.817, 46.951, 31.054],
+           "titao": [20/Tc, 1570/Tc, 4700/Tc]}
+
+    CP2 = {"ao": 4,
+           "ao_exp": [28.817, 46.951, 31.054],
+           "exp": [20, 1570, 4700]}
 
     f = 8.314472
     CP1 = {"ao": 275.1/f,
            "ao_sinh": [612.9/f], "sinh": [1829.6],
            "ao_cosh": [413.0/f], "cosh": [802.6]}
+
+    thol = {
+        "__type__": "Helmholtz",
+        "__name__": "Helmholtz equation of state for "
+                    "octamethyltrisiloxane of Thol (2017).",
+        "__doi__": {
+            "autor": "Thol, M., Dubberke, F.H., Baumhögger, E., Vrabec, J., "
+                     "Span, R.",
+            "title": "Speed of Sound Measuements and Fundamental Equations "
+                     "State for Octamethyltrisiloxane and "
+                     "Decamethyltetrasiloxane",
+            "ref": "J. Chem. Eng. Data 62(9) (2017) 2633-2648",
+            "doi": "10.1021/acs.jced.7b00092"},
+
+        "R": 8.3144598,
+        "cp": CP2,
+        "ref": "NBP",
+
+        "Tmin": Tt, "Tmax": 600, "Pmax": 130000,
+
+        "nr1": [0.05039724, 1.189992, -2.468723, -0.743856, 0.4434056],
+        "d1": [4, 1, 1, 2, 3],
+        "t1": [1, 0.188, 1.03, 0.7, 0.464],
+
+        "nr2": [-1.371359, -1.529621, 0.4445898, -1.009921, -0.05903694],
+        "d2": [1, 3, 2, 2, 7],
+        "t2": [2.105, 1.376, 0.8, 1.8, 1.005],
+        "c2": [2, 2, 1, 2, 1],
+        "gamma2": [1]*5,
+
+        "nr3": [3.515188, 0.08367608, 1.646856, -0.2851917, -2.457571],
+        "d3": [1, 1, 3, 2, 2],
+        "t3": [0.7, 0.66, 1.138, 1.56, 1.31],
+        "alfa3": [0.986, 1.715, 0.837, 1.312, 1.191],
+        "beta3": [0.966, 0.237, 0.954, 0.861, 0.909],
+        "gamma3": [1.25, 1.438, 0.894, 0.9, 0.899],
+        "epsilon3": [0.928, 2.081, 0.282, 1.496, 0.805]}
 
     colonna = {
         "__type__": "Helmholtz",
@@ -53,7 +102,7 @@ class MDM(MEoS):
                              " [(CH3)3-Si-O1/2]2-[O-Si-(CH3)2]i=1,…,3, and "
                              "[O-Si-(CH3)2]6",
                     "ref": "Fluid Phase Equilibria 263:115-130, 2008",
-                    "doi":  "10.1016/j.fluid.2007.10.001"},
+                    "doi": "10.1016/j.fluid.2007.10.001"},
 
         "R": 8.314472,
         "cp": CP1,
@@ -73,7 +122,7 @@ class MDM(MEoS):
         "c2": [1, 1, 2, 2, 3, 3],
         "gamma2": [1]*6}
 
-    eq = colonna,
+    eq = thol, colonna
 
     _surface = {
         "__doi__": {
@@ -86,14 +135,59 @@ class MDM(MEoS):
 
     _vapor_Pressure = {
         "eq": 3,
-        "n": [-0.85589e1, 0.20278e1, -0.28501e1, -0.64397e1, -0.85460e1],
-        "t": [1.0, 1.5, 2.3, 4.0, 13.0]}
+        "n": [-8.8192, 4.0952, -4.062, -6.208, -3.212],
+        "t": [1, 1.5, 1.9, 3.71, 14.6]}
     _liquid_Density = {
         "eq": 1,
-        "n": [0.54145, -0.27650e-1, 0.41558e1, -0.19104e1, 0.67606],
-        "t": [0.12, 0.36, 0.6, 0.8, 2.5]}
+        "n": [7.016, -13.924, 20.84, -16.64, 5.906],
+        "t": [0.54, 0.9, 1.3, 1.73, 2.2]}
     _vapor_Density = {
         "eq": 2,
-        "n": [-0.16483e1, -0.71410e1, -0.23088e2, -0.70554e2, 0.19938e1,
-              -0.20193e3],
-        "t": [0.296, 0.905, 2.8, 5.9, 12.0, 13.0]}
+        "n": [-5.3686, -11.85, -16.64, -52.26, -125.6, -235.7],
+        "t": [0.515, 4.58, 2.06, 5.25, 11.3, 21.6]}
+
+
+class Test(TestCase):
+
+    # Reference state fail with last decimal number
+    def test_thol(self):
+        # Table 13, Pag. 2646
+        st = MDM(T=320, rhom=0.0001)
+        self.assertEqual(round(st.P.MPa, 10), 0.0002659237)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 343.4178)
+        self.assertEqual(round(st.w, 4), 107.3143)
+        # self.assertEqual(round(st.hM.Jmol, 3), -2908.074)
+        # self.assertEqual(round(st.sM.JmolK, 5), 28.42609)
+        self.assertEqual(round(st.aM.Jmol, 2), -14663.66)
+
+        st = MDM(T=320, rhom=3.5)
+        self.assertEqual(round(st.P.MPa, 5), 25.48165)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 427.6187)
+        self.assertEqual(round(st.w, 3), 1025.009)
+        # self.assertEqual(round(st.hM.Jmol, 2), -44465.80)
+        self.assertEqual(round(st.sM.JmolK, 4), -140.7280)
+        # self.assertEqual(round(st.aM.Jmol, 3), -6713.301)
+
+        st = MDM(T=500, rhom=0.05)
+        self.assertEqual(round(st.P.MPa, 7), 0.1919677)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 464.1575)
+        self.assertEqual(round(st.w, 4), 123.7605)
+        # self.assertEqual(round(st.hM.Jmol, 2), 68249.50)
+        self.assertEqual(round(st.sM.JmolK, 4), 149.4223)
+        self.assertEqual(round(st.aM.Jmol, 2), -10301.03)
+
+        st = MDM(T=500, rhom=3)
+        self.assertEqual(round(st.P.MPa, 5), 34.98485)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 516.4433)
+        self.assertEqual(round(st.w, 4), 752.6067)
+        # self.assertEqual(round(st.hM.Jmol, 2), 42566.70)
+        # self.assertEqual(round(st.sM.JmolK, 5), 66.33294)
+        self.assertEqual(round(st.aM.Jmol, 3), -2261.391)
+
+        st = MDM(T=575, rhom=2)
+        self.assertEqual(round(st.P.MPa, 6), 3.511622)
+        # self.assertEqual(round(st.cpM.JmolK, 4), 597.2631)
+        self.assertEqual(round(st.w, 4), 204.4220)
+        # self.assertEqual(round(st.hM.Jmol, 2), 81168.03)
+        self.assertEqual(round(st.sM.JmolK, 4), 160.0291)
+        self.assertEqual(round(st.aM.Jmol, 2), -12604.53)
