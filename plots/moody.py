6ce1e7ef607aa04b87368fc3ffa0b52c9fe6d367
Juan José Gómez Romera <jjgomera@gmail.com>
2025-06-24 15:46:54 +0200

    Add paragraph title for api reference

--- plots/moody.py
+++ plots/moody.py
@@ -21,6 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 .. include:: moody.rst
 
 
+API Reference
+-------------
+
 The module include all related moody chart functionality
     * :class:`Moody`: Chart dialog
     * :func:`calculate`: Calculate procedure
