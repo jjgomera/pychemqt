cc7ea4d4e70b4e34bb10161f23cb86fae932c64d
Juan José Gómez Romera <jjgomera@gmail.com>
2026-02-05 10:38:38 +0100

    Remame equipment/accesories to equipment/widget

---
+++ equipment/widget/gui.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''Pychemqt, Chemical Engineering Process simulator
+Copyright (C) 2009-2025, Juan José Gómez Romera <jjgomera@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
+
+from tools.qt import QtWidgets
+
+
+class ToolGui(QtWidgets.QWidget):
+    """Parent widget with common functionality for equipment tools"""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        lyt = QtWidgets.QGridLayout(self)
+        self.check = QtWidgets.QCheckBox(self.title)
+        self.check.toggled.connect(self.setEnabled)
+        lyt.addWidget(self.check, 0, 1)
+        lyt.addItem(QtWidgets.QSpacerItem(
+            20, 20, QtWidgets.QSizePolicy.Policy.Fixed,
+            QtWidgets.QSizePolicy.Policy.Fixed), 1, 1)
+
+        self.loadUI()
+
+        lyt.addItem(QtWidgets.QSpacerItem(
+            10, 10, QtWidgets.QSizePolicy.Policy.Expanding,
+            QtWidgets.QSizePolicy.Policy.Expanding), 10, 3)
+
+        self.setEnabled(False)
+
+    def setEnabled(self, boolean):
+        """Toggled enable/disable state for all children widget except
+        checkbox used to change this"""
+        for wdg in self.children():
+            if wdg is not self.check and wdg is not self.layout():
+                wdg.setEnabled(boolean)
+
+    def isChecked(self):
+        """Level up check state as global value for widget"""
+        return self.check.isChecked()
+
+    def setChecked(self, boolean):
+        self.check.setChecked(boolean)
+
+    def loadUI(self):
+        """Rewrite in child class to add widget"""
+
