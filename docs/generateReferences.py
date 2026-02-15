80659b12270b889f5b93b4b38117142410a1116d
Juan José Gómez Romera <jjgomera@gmail.com>
2026-02-15 13:56:09 +0100

    Trying to avoid rtd build error by autodoc unspecific error #2

--- docs/generateReferences.py
+++ docs/generateReferences.py
@@ -84,7 +84,7 @@ for library in lib.__all__:
         print("="*(len(library)+4+7), file=file)
         print("", file=file)
         print(".. automodule:: lib.%s" % library, file=file)
-        if module in ("coolProp", "refProp"):
+        if library in ("coolProp", "refProp"):
             print("    :ignore-module-all:", file=file)
 
         if hasattr(module, "__doi__") and module.__doi__:
