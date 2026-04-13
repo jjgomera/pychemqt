63a34fc1e5a120d1c7f775d210caf017a19dca32
Juan José Gómez Romera <jjgomera@gmail.com>
2026-04-13 21:58:13 +0200

    Add Vicente and Sethumadhavan correlation for nusselt number and friction factor for corrugated tube

--- equipment/widget/rib.py
+++ equipment/widget/rib.py
@@ -19,8 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
 
 from functools import partial
-from math import pi, log, cos, atan
+from math import log
 
+from scipy.optimize import newton
 from tools.qt import QtCore, QtWidgets, translate
 
 from equipment.widget.gui import ToolGui, CallableEntity
@@ -36,17 +37,192 @@ __doi__ = {
                   "characteristics of horizontal tubes with helical rib",
          "ref": "Energy Conv. Management 47(18-19) (2006) 3031-3044",
          "doi": "10.1016/j.enconman.2006.03.023"},
-    # 2:
+    2:
+        {"autor": "Vicente, P.G., García, A., Viedma, A.",
+         "title": "Experimental investigation on heat transfer and frictional "
+                  "characteristics of spirally corrugated tubes in turbulent "
+                  "flow at different Prandtl numbers",
+         "ref": "Int. J. Heat Mass Transfer 47(4) (2004) 671-681",
+         "doi": "10.1016/j.ijheatmasstransfer.2003.08.005"},
+    3:
+        {"autor": "Vicente, P.G., García, A., Viedma, A.",
+         "title": "Mixed convection heat transfer and isothermal pressure "
+                  "drop in corruageted tubes for laminar and transition flow",
+         "ref": "Int. Comm. Heat Mass Transfer 31(5) (2004) 651-662",
+         "doi": "10.1016/S0735-1933(04)00052-1"},
+    4:
+        {"autor": "Sethumadhavan, R., Raja Rao, M.",
+         "title": "Turbulent Flow Friction and Heat Transfer Characteristics "
+                  "of Single and Multistart Spirally Enghanced Tubes",
+         "ref": "J. Heat Transfer 108(1) (1986) 55-61",
+         "doi": "10.1115/1.3246905"},
+
+    # 5:
     #     {"autor": "",
     #      "title": "",
     #      "ref": "",
-    #      "doi": ""}
+    #      "doi": ""},
         }
 
 
 # Friction factor correlations
+@refDoc(__doi__, [2, 3])
+def f_corrugated_Vicente(Re, Di, p, h):
+    """Calculate friction factor for a corrugated tube using the Vicente et al.
+    Tcorrelation (2004).
+
+    Parameters
+    ----------
+    Re : float
+        Reynolds number, [-]
+    Di : float
+        Internal diameter of tube, [m]
+    p : float
+        Helical pitch for twist of 2π radians (360º), [m]
+    h : float
+        Roughness height, [m]
+
+    Returns
+    -------
+    f : float
+        Friction factor, [-]
+    """
+    phi = h**2/p/Di
+
+    if Re > 2000:
+        # Turbulent flow,  Eq 2
+        f = 1.53 * phi**0.46 / Re**0.16
+    else:
+        # Laminar flow, Eq 3 in [3]_
+        f = 29.8 * phi**0.11 / Re**0.97
+
+    return f
+
+@refDoc(__doi__, [4])
+def f_corrugated_Sethumadhavan(Re, Di, P, h):
+    """Calculate friction factor for a corrugated pipe using the
+    Sethumadhavan-Raja Rao correlation (1986).
+
+    Parameters
+    ----------
+    Re : float
+        Reynolds number, [-]
+    D : float
+        Internal diameter of tube, [m]
+    P : float
+        helical pitch for twist of 2π radians (360º), [m]
+    h : float
+        Roughness height, [m]
+
+    Returns
+    -------
+    f : float
+        Friction factor, [-]
+    """
+    if Re < 5000:
+        raise NotImplementedError("Input out of bound")
+
+    # Helix angle
+    Deq = Di-h
+
+    # Eq 13
+    def f_res(f):
+        """Iterative solution of intrinsic equation"""
+
+        R = 2**0.5/f + 2.5*log(2*h/Deq) + 3.75
+        h_ = h/Deq * Re * (f/2)**0.5
+        return R*h**2/(P*Deq)**0.33 - 0.40*h_**0.164
+
+    fo = f_corrugated_Vicente(Re, Di, P, h)
+    f = newton(f_res, fo)
+
+    if isinstance(f, complex):
+        raise ValueError("Solution don't converge")
+
+    return f
+
+
+# Heat Transfer coefficient correlations
+@refDoc(__doi__, [2, 3])
+def Nu_corrugated_Vicente(Re, Pr, Di, p, h):
+    """Calculate friction factor for a corrugated tube using the Vicente et al.
+    correlation (2004).
+
+    Parameters
+    ----------
+    Re : float
+        Reynolds number, [-]
+    Pr : float
+        Prandtl number, [-]
+    Di : float
+        Internal diameter of tube, [m]
+    p : float
+        Helical pitch for twist of 2π radians (360º), [m]
+    h : float
+        Roughness height, [m]
+
+    Returns
+    -------
+    Nu : float
+        Nusselt number, [-]
+    """
+    phi = h**2/p/Di
+
+    if Re > 2000:
+        # Turbulent flow, Eq 9
+        Nu = 0.374 * phi**0.25 * (Re-1500)**0.74 * Pr**0.44
+    else:
+        # Laminar flow, for simplicity use the constant infinite value to avoid
+        # Rayleigh input parameters
+        Nu = 4.36
+
+    return Nu
+
+
+@refDoc(__doi__, [4])
+def Nu_corrugated_Sethumadhavan(Re, Pr, Di, P, h):
+    """Calculate Nusselt number for a corrugated pipe using the
+    Sethumadhavan-Raja Rao correlation (1986).
+
+    Parameters
+    ----------
+    Re : float
+        Reynolds number, [-]
+    Pr : float
+        Prandtl number, [-]
+    Di : float
+        Internal diameter of tube, [m]
+    p : float
+        Helical pitch for twist of 2π radians (360º), [m]
+    h : float
+        Roughness height, [m]
+
+    Returns
+    -------
+    Nu : float
+        Nusselt number, [-]
+    """
+    if Re < 5000:
+        raise NotImplementedError("Input out of bound")
+
+    Deq = Di-h
+
+    f = f_corrugated_Vicente(Re, Di, P, h)
+    h_ = h/Deq * Re * (f/2)**0.5
+    R = 2**0.5/f + 2.5*log(2*h/Deq) + 3.75
+
+    # Eq 15
+    G = 8.6 * h_**0.13 * Pr**-0.55
+
+    # Eq 7
+    St = 1/((((G-R)*(f/2)**0.5)+1)*2/f)
+
+    return St*Re*Pr
+
+
+# Rib correlation
 @refDoc(__doi__, [1])
-def f_rib_Naphon(Re, Di, p, Xi):
+def f_rib_Naphon(Re, Di, p, h):
     """Calculate friction factor for a tube with helical rib using the Naphon
     correlation (2006).
 
@@ -58,7 +234,7 @@ def f_rib_Naphon(Re, Di, p, Xi):
         Internal diameter of tube, [m]
     p : float
         Helical rib pitch for twist of 2π radians (360º), [m]
-    Xi : float
+    h : float
         Helical rib depth, [m]
 
     Returns
@@ -67,14 +243,13 @@ def f_rib_Naphon(Re, Di, p, Xi):
         Friction factor, [-]
     """
     # Eq 11
-    f = 7.85 / Re**0.21 * (Xi/Di)**1.68 / (p/Di)**0.54
+    f = 7.85 / Re**0.21 * (h/Di)**1.68 / (p/Di)**0.54
 
     return f
 
 
-# Heat Transfer coefficient correlations
 @refDoc(__doi__, [1])
-def Nu_rib_Naphon(Re, Pr, Di, p, Xi):
+def Nu_rib_Naphon(Re, Pr, Di, p, h):
     """Calculate Nusselt number for a tube with helical rib using the Naphon
     correlation (2006).
 
@@ -88,7 +263,7 @@ def Nu_rib_Naphon(Re, Pr, Di, p, Xi):
         Internal diameter of tube, [m]
     p : float
         Helical rib pitch for twist of 2π radians (360º), [m]
-    Xi : float
+    h : float
         Helical rib depth, [m]
 
     Returns
@@ -97,7 +272,7 @@ def Nu_rib_Naphon(Re, Pr, Di, p, Xi):
         Nusselt number, [-]
     """
     # Eq 8
-    Nu = 44.26 * (Re-1500)**0.27 / Pr**0.26 * (Xi/Di)**0.89 / (p/Di)**0.96
+    Nu = 44.26 * (Re-1500)**0.27 / Pr**0.26 * (h/Di)**0.89 / (p/Di)**0.96
 
     return Nu
 
@@ -109,7 +284,7 @@ class Rib(CallableEntity):
     ----------
     p : float
         Helical rib pitch for twist of 2π radians (360º), [m]
-    Xi : float
+    h : float
         Helical rib depth, [m]
     """
 
@@ -117,7 +292,7 @@ class Rib(CallableEntity):
     msg = ""
     kw = {
         "p": 0,
-        "Xi": 0}
+        "h": 0}
 
     valueChanged = QtCore.pyqtSignal(object)
     inputChanged = QtCore.pyqtSignal(object)
@@ -129,7 +304,7 @@ class Rib(CallableEntity):
             self.msg = translate("equipment", "undefined rib pitch")
             self.status = 0
             return False
-        if not self.kw["Xi"]:
+        if not self.kw["h"]:
             self.msg = translate("equipment", "undefined rib depth")
             self.status = 0
             return False
@@ -141,18 +316,18 @@ class Rib(CallableEntity):
     def calculo(self):
         """Definition of twisted tape inserts for annuli sections"""
         self.p = self.kw["p"]
-        self.Xi = self.kw["Xi"]
+        self.h = self.kw["h"]
 
         self.valueChanged.emit(self)
 
     def Nu(self, Re, Pr, Di):
         """Calculate nusselt number"""
-        Nu = Nu_rib_Naphon(Re, Pr, Di, self.p, self.Xi)
+        Nu = Nu_rib_Naphon(Re, Pr, Di, self.p, self.h)
         return Nu
 
     def f(self, Re, Di):
         """Calculate friction factor"""
-        f = f_rib_Naphon(Re, Di, self.p, self.Xi)
+        f = f_rib_Naphon(Re, Di, self.p, self.h)
         return f
 
 
@@ -175,9 +350,9 @@ class UI_Rib(ToolGui):
         lyt.addWidget(self.p, 2, 2)
         label = QtWidgets.QLabel("Rib depth")
         lyt.addWidget(label, 3, 1)
-        self.Xi = Entrada_con_unidades(Length, "Thickness")
-        self.Xi.valueChanged.connect(partial(self.changeParams, "Xi"))
-        lyt.addWidget(self.Xi, 3, 2)
+        self.h = Entrada_con_unidades(Length, "Thickness")
+        self.h.valueChanged.connect(partial(self.changeParams, "h"))
+        lyt.addWidget(self.h, 3, 2)
 
         self.Entity.valueChanged.connect(self.valueChanged.emit)
         self.Entity.inputChanged.connect(self.populate)
